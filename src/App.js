9a1fe6a9e1bb6eb485cc55fdd66b9881a7675b2e
sverma507 <sverma70568@gmail.com>
2024-05-14 11:56:15 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import axios from 'axios'
 function App() {
+  const [data,setData]= useState({
+    email:'',
+    name:'',
+    number:'',
+    salary:''
+  })
+
+  const go_get=(e)=>{
+    // console.log("e=>",e.target);
+    const {name,value}=e.target;
+    setData((prev)=>{
+      return({
+        ...prev,
+        [name]:value
+      })
+    })
+
+  }
+
+  const submit =(e)=>{
+    e.preventDefault();
+    console.log("data=>", data)
+      axios.post('http://localhost:4000/adduser', data)
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    
+  
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={data.email} onChange={(e)=>{go_get(e)}} />
+        </div>
+        <div class="mb-3">
+          <label for="name" class="form-label">Name</label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='name' value={data.name} onChange={(e)=>{go_get(e)}} />
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Number</label>
+          <input type="number" class="form-control" id="exampleInputPassword1"  name='number' value={data.number} onChange={(e)=>{go_get(e)}} />
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Salary</label>
+          <input type="number" class="form-control" id="exampleInputPassword1" name='salary' value={data.salary} onChange={(e)=>{go_get(e)}} />
+        </div>
+       
+        <button type="submit" class="btn btn-primary" onClick={(e)=>{submit(e)}}>Submit</button>
+      </form>
     </div>
   );
 }
